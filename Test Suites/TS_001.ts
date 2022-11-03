cd0792629a493cc17b053736f7e5868aafa4ab27
Kavindu Dilshan <kavindu.dilshan@ifs.com>
2022-11-03 13:05:08 +0530

    TS added

---
+++ Test Suites/TS_001.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_001</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>c141a433-3467-44b4-a1ae-863d7c70806a</testSuiteGuid>
+</TestSuiteEntity>
